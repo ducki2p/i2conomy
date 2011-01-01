15b28254402e6001f7f99047bba17ccb15933ffe
duck <duck@mail.i2p>
2011-01-01 18:10:43 +0100

    3: Balance (basic)

--- src/i2conomy/mint.clj
+++ src/i2conomy/mint.clj
@@ -1,18 +1,57 @@
 (ns i2conomy.mint)
 
+(def accounts (ref (sorted-map)))
+
 (def transfers (ref ()))
 
+(defrecord Account [name balance-ref])
+
 (defrecord Transfer [timestamp from to currency amount memo])
+
+(defn balance
+  "Gets the balance of an account"
+  [name]
+    (if-let [account (get @accounts name)]
+      @(:balance-ref account)))
+
+(defn create-account
+  "Creates a new account"
+  [name]
+  (dosync
+    (let [now (java.util.Date.)
+          account (Account. name (ref 0))]
+      (alter accounts assoc name account))))
+
+(defn- update-balance
+  "Modifies the balance of an account, to be used within a transation"
+  [account amount]
+  (let [balance-ref (:balance-ref account)]
+    (alter balance-ref + amount)))
 
 (defn pay
   "Makes a payment between accounts"
   [from to currency amount memo]
-    (dosync
-      (let [now (java.util.Date.)]
-        (alter transfers conj (Transfer. now from to currency amount memo)))))
+  (dosync
+    (let [now (java.util.Date.)
+          transfer (Transfer. now from to currency amount memo)
+          from-account (get @accounts from)
+          to-account (get @accounts to)]
+      (cond
+        (nil? from-account)
+          (throw (IllegalArgumentException.
+                  (str "account " from " does not exist")))
+        (nil? to-account)
+          (throw (IllegalArgumentException.
+                  (str "account " to " does not exist")))
+        :else (do
+          (alter transfers conj transfer)
+          (update-balance from-account (- amount))
+          (update-balance to-account amount))))))
 
 (defn reset-all!
-  "Reset all transfers, use with care"
+  "Resets all accounts and transfers, use with care!"
   []
   (dosync
+    (ref-set accounts (sorted-map))
     (ref-set transfers ())))
+
--- test/i2conomy/test/mint.clj
+++ test/i2conomy/test/mint.clj
@@ -1,14 +1,30 @@
 (ns i2conomy.test.mint
   (:use [i2conomy.mint] :reload)
-  (:use [clojure.test]))
+  (:use [clojure.test :only [deftest is]]))
 
 (deftest no-payments
   (do
     (reset-all!)
+    (is (= 0 (count @accounts)))
     (is (= 0 (count @transfers)))))
 
 (deftest single-payment
   (do
     (reset-all!)
+    (create-account "alice")
+    (create-account "bob")
     (pay "alice" "bob" "alice" 100 "groceries")
-      (is (= 1 (count @transfers)))))
+      (is (= 1 (count @transfers))))
+      (is (= -100 (balance "alice")))
+      (is (= 100 (balance "bob"))))
+
+(deftest balance-nonexisting
+  (do
+    (reset-all!)
+    (is (= nil (balance "alice")))))
+
+(deftest balance-new-account
+  (do
+    (reset-all!)
+    (create-account "alice")
+    (is (= 0 (balance "alice")))))
